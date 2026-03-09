e7956f6966bfe9c3e6f74ee9a4d77b6007eb0243
Jeevitha-Github-04 <jeevithajeevi1426@gmail.com>
2026-03-10 01:17:03 +0530

    updated

--- EMC-Kart-Ui/src/main.jsx
+++ EMC-Kart-Ui/src/main.jsx
@@ -1,3 +1,4 @@
+import { HashRouter } from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -6,7 +7,9 @@ import { Provider } from 'react-redux';
 import store from './store/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
+  </HashRouter>
 )
